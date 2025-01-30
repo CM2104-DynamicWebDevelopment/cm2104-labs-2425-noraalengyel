37ceffbc50cd318b4519561d2888fe467485a538
Nora Lengyel <n.lengyel@rgu.ac.uk>
2025-01-30 11:24:50 +0000

    ssgsgrs

--- lab_11/server.js
+++ lab_11/server.js
@@ -22,7 +22,6 @@ app.get('/joke', function(req,res){
 app.get('/add', function(req,res){
     var x = req.query.x;
     var y = req.query.y;
-    console.log(parseInt(x));
     res.send("X + Y = " + x + " " + y); 
 })
 app.listen(8080);
