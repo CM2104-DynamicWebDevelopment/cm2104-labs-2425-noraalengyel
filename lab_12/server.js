8a356984f5635466ab6896aae43f4c80f6fd6ef3
Nora Lengyel <n.lengyel@rgu.ac.uk>
2025-02-06 11:26:38 +0000

    formatting the result

--- lab_12/server.js
+++ lab_12/server.js
@@ -28,7 +28,25 @@ spotifyApi.clientCredentialsGrant().then( //connect to API
 async function getTracks(searchterm, res) {
     spotifyApi.searchTracks(searchterm)
         .then(function(data){
-            res.send(JSON.stringify(data.body));
+            var tracks = data.body.tracks.items
+            //lets set up an empty string to act as the response
+            var HTMLResponse = "";
+            //now lets run through all the items
+            //this is a for loop
+            for (var i=0; i<tracks.length; i++){
+                var track = tracks[i];
+                console.log(track.name);
+
+                HTMLResponse = HTMLResponse + 
+                "<div>" + 
+                    "<h2>" + track.name + "</h2>" + 
+                    "<h4>" + track.artists[0].name + "</h4>" + 
+                    "<img src='" + track.album.images[0].url + "'>" + 
+                    "<a href='" + track.external_urls.spotify + "'> Track Details </a>" + 
+                "</div>";
+                console.log(HTMLResponse);
+            }
+            res.send(HTMLResponse);
         }, function(err){
             console.log(err);
     });  
