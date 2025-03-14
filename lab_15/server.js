19405f1ad6013232fe0abd518cf3709183421daa
Nora Lengyel <n.lengyel@rgu.ac.uk>
2025-03-14 12:49:37 +0000

    task4

--- lab_15/server.js
+++ lab_15/server.js
@@ -40,50 +40,34 @@ var db;
 connectDB();
 //this is our connection to the mongo db, ts sets the variable db as our database
 async function connectDB() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected successfully to server');
-    db = client.db(dbname);
-    //everything is good lets start
-    app.listen(8080);
-    console.log('Listening for connections on port 8080');
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  db = client.db(dbname);
+  //everything is good lets start
+  app.listen(8080);
+  console.log('Listening for connections on port 8080');
 }
 
 
 //********** GET ROUTES - Deal with displaying pages ***************************
 
-//this is our root route
-/*app.get('/', function(req, res) {
-  //if the user is not logged in redirect them to the login page
-  if(!req.session.loggedin){res.redirect('/login');return;}
-
-  //otherwise perfrom a search to return all the documents in the people collection
-  db.collection('people').find().toArray(function(err, result) {
-    if (err) throw err;
-    //the result of the query is sent to the users page as the "users" array
-    res.render('pages/users', {
-      users: result
-    })
-  });
-
-});*/
-
 //this is our login route, all it does is render the login.ejs page.
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
   res.render('pages/login');
 });
 
 
-app.get('/profile', function(req, res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
-  
-  
+app.get('/profile', function (req, res) {
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
+
+
   var uname = req.query.username;
-  
- 
-  db.collection('people').findOne({"login.username": uname}, function(err, result) {
+
+
+  db.collection('people').findOne({ "login.username": uname }, function (err, result) {
     if (err) throw err;
-   
+
 
 
     res.render('pages/profile', {
@@ -93,18 +77,18 @@ app.get('/profile', function(req, res) {
 
 });
 //adduser route simply draws our adduser page
-app.get('/adduser', function(req, res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
+app.get('/adduser', function (req, res) {
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
   res.render('pages/adduser')
 });
 //remuser route simply draws our remuser page
-app.get('/remuser', function(req, res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
+app.get('/remuser', function (req, res) {
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
   res.render('pages/remuser')
 });
 //logour route cause the page to Logsout.
 //it sets our session.loggedin to false and then redirects the user to the login
-app.get('/logout', function(req, res) {
+app.get('/logout', function (req, res) {
   req.session.loggedin = false;
   req.session.destroy();
   res.redirect('/');
@@ -112,27 +96,46 @@ app.get('/logout', function(req, res) {
 
 //adding a section to the top of the index page to welcome the currently logged in user
 //this is the root route
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
   //check we are logged in
-  if(!req.session.loggedin){res.redirect('/login');return;}
-   
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
+
   var currentUser = req.session.currentUser;
 
-  db.collection('people').find().toArray(function(err,result){
+  db.collection('people').find().toArray(function (err, result) {
     if (err) throw err;
     //var currentlyLoggedInHooman = req.session.currentUser
 
-    db.collection('people').findOne({"login.username": currentUser}, function(err, userresult){
+    db.collection('people').findOne({ "login.username": currentUser }, function (err, userresult) {
       //the result of the query is sent to the users page a the "users" array
       res.render('pages/users', {
-      users: result,
-      user: userresult
-    })
+        users: result,
+        user: userresult
+      })
     })
 
-    
+
   });
 
+});
+
+//adding the /updateuser that will render an update.js template
+app.get('/updateuser', function (req, res) {
+  //check we are logged in
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
+
+  var uname = req.query.username;
+
+  db.collection('people').findOne({ "login.username": uname }, function (err, result) {
+    if (err) throw err;
+
+    if (!result) {
+      res.redirect('/');
+      return;
+    }
+
+    res.render('pages/update', { user: result });
+  });
 });
 
 
@@ -142,41 +145,43 @@ app.get('/', function(req,res){
 
 //the dologin route detasl with the data from the login screen.
 //the post variables, username and password ceom from the form on the login page.
-app.post('/dologin', function(req, res) {
+app.post('/dologin', function (req, res) {
   console.log(JSON.stringify(req.body))
   var uname = req.body.username;
   var pword = req.body.password;
 
   //var currentuser = req.session.currentuser;
 
-  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+  db.collection('people').findOne({ "login.username": uname }, function (err, result) {
     if (err) throw err;
 
 
-    if(!result){res.redirect('/login');return}
+    if (!result) { res.redirect('/login'); return }
 
 
 
-    if(result.login.password == pword){ req.session.loggedin = true;
+    if (result.login.password == pword) {
+      req.session.loggedin = true;
       req.session.currentUser = uname
       //console.log("current user: " + req.session.currentUser)
-      res.redirect('/') }
+      res.redirect('/')
+    }
 
 
 
-    else{res.redirect('/login')}
+    else { res.redirect('/login') }
   });
 });
 
 //the delete route deals with user deletion based on entering a username
-app.post('/delete', function(req, res) {
+app.post('/delete', function (req, res) {
   //check we are logged in.
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
   //if so get the username variable
   var uname = req.body.username;
 
   //check for the username added in the form, if one exists then you can delete that doccument
-  db.collection('people').deleteOne({"login.username":uname}, function(err, result) {
+  db.collection('people').deleteOne({ "login.username": uname }, function (err, result) {
     if (err) throw err;
     //when complete redirect to the index
     res.redirect('/');
@@ -197,30 +202,59 @@ app.post('/delete', function(req, res) {
 //"picture":{"large":"https://randomuser.me/api/portraits/women/42.jpg","medium":"https://randomuser.me/api/portraits/med/women/42.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/42.jpg"},
 //"nat":"GB"}
 
-app.post('/adduser', function(req, res) {
+app.post('/adduser', function (req, res) {
   //check we are logged in
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
 
   //we create the data string from the form components that have been passed in
 
-var datatostore = {
-"gender":req.body.gender,
-"name":{"title":req.body.title,"first":req.body.first,"last":req.body.last},
-"location":{"street":req.body.street,"city":req.body.city,"state":req.body.state,"postcode":req.body.postcode},
-"email":req.body.email,
-"login":{"username":req.body.username,"password":req.body.password},
-"dob":req.body.dob,"registered":Date(),
-"picture":{"large":req.body.large,"medium":req.body.medium,"thumbnail":req.body.thumbnail},
-"nat":req.body.nat}
+  var datatostore = {
+    "gender": req.body.gender,
+    "name": { "title": req.body.title, "first": req.body.first, "last": req.body.last },
+    "location": { "street": req.body.street, "city": req.body.city, "state": req.body.state, "postcode": req.body.postcode },
+    "email": req.body.email,
+    "login": { "username": req.body.username, "password": req.body.password },
+    "dob": req.body.dob, "registered": Date(),
+    "picture": { "large": req.body.large, "medium": req.body.medium, "thumbnail": req.body.thumbnail },
+    "nat": req.body.nat
+  }
 
 
-//once created we just run the data string against the database and all our new data will be saved/
-  db.collection('people').insertOne(datatostore, function(err, result) {
+  //once created we just run the data string against the database and all our new data will be saved/
+  db.collection('people').insertOne(datatostore, function (err, result) {
     if (err) throw err;
     console.log('saved to database')
     //when complete redirect to the index
     res.redirect('/')
   })
 });
+
+
+//adding /doupdate route to do the actual update
+app.post('/doupdate', function (req, res) {
+  //check we are logged in
+  if (!req.session.loggedin) { res.redirect('/login'); return; }
+
+  var uname = req.body.username;
+
+  var updatedData = {
+    "gender": req.body.gender,
+    "name": { "title": req.body.title, "first": req.body.first, "last": req.body.last },
+    "location": { "street": req.body.street, "city": req.body.city, "state": req.body.state, "postcode": req.body.postcode },
+    "email": req.body.email,
+    "login": { "username": req.body.username, "password": req.body.password },
+    "dob": req.body.dob, "registered": Date(),
+    "picture": { "large": req.body.large, "medium": req.body.medium, "thumbnail": req.body.thumbnail },
+    "nat": req.body.nat
+  };
+
+  db.collection('people').insertOne(datatostore, function (err, result) {
+    if (err) throw err;
+    console.log('user updated');
+    //when complete redirect to the index
+    res.redirect('/')
+  });
+});
+
 
 
